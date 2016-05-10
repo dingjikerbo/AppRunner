da3f21c03925367cbb135d1fc30b5f2490f1a2b2
dingjikerbo <dingjikerbo@gmail.com>
2016-05-10 22:08:29 +0800

    delete all

--- library/src/main/java/com/dingjikerbo/library/BaseActivity.java
+++
@@ -1,9 +0,0 @@
-package com.dingjikerbo.library;
-
-import android.support.v4.app.FragmentActivity;
-
-/**
- * Created by dingjikerbo on 16/3/13.
- */
-public class BaseActivity extends FragmentActivity {
-}
